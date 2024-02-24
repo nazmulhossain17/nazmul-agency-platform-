45bf6e842846e6751efea01765ab278f085862c3
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2024-02-24 19:23:51 +0600

    update

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -9,14 +9,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { stripe } from "@/lib/stripe";
 import clsx from "clsx";
 import { pricingCards } from "@/lib/constants";
+import { features } from "process";
 export default async function Home() {
-  const prices = await stripe.prices.list({
-    // product: process.env.NEXT_PLURA_PRODUCT_ID,
-    // active: true,
-  });
   return (
     <>
       <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col ">
@@ -25,7 +21,7 @@ export default async function Home() {
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
         {/* <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div> */}
 
-        <p className="text-center">Run your agency, in one place</p>
+        {/* <p className="text-center">Run your agency, in one place</p> */}
         <div className="bg-gradient-to-r from-destructive to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-9xl font-bold text-center md:text-[300px]">
             Agency
@@ -50,55 +46,42 @@ export default async function Home() {
           ready to commit you can get started for free.
         </p>
         <div className="flex  justify-center gap-4 flex-wrap mt-6">
-          {prices.data.map((card) => (
-            //WIP: Wire up free product from stripe
+          {pricingCards.map((card) => (
             <Card
-              key={card.nickname}
+              key={card.title}
               className={clsx("w-[300px] flex flex-col justify-between", {
-                "border-2 border-primary": card.nickname === "Unlimited Saas",
+                "border-2 border-primary": card.title === "Unlimited Sass",
               })}
             >
               <CardHeader>
                 <CardTitle
                   className={clsx("", {
-                    "text-muted-foreground": card.nickname !== "Unlimited Saas",
+                    "text-muted-foreground": card.title === "Unlimited Sass",
                   })}
                 >
-                  {card.nickname}
+                  {card.title}
                 </CardTitle>
-                <CardDescription>
-                  {
-                    pricingCards.find((c) => c.title === card.nickname)
-                      ?.description
-                  }
-                </CardDescription>
+                <CardDescription>{card.description}</CardDescription>
               </CardHeader>
               <CardContent>
-                <span className="text-4xl font-bold">
-                  {card.unit_amount && card.unit_amount / 100}
-                </span>
-                <span className="text-muted-foreground">
-                  <span>/ {card.recurring?.interval}</span>
-                </span>
+                <span className="text-4xl font-bold">{card.price}</span>
+                <span className="text-4xl font-bold">/m</span>
               </CardContent>
               <CardFooter className="flex flex-col items-start gap-4">
                 <div>
-                  {pricingCards
-                    .find((c) => c.title === card.nickname)
-                    ?.features.map((feature) => (
-                      <div key={feature} className="flex gap-2">
-                        <Check />
-                        <p>{feature}</p>
-                      </div>
-                    ))}
+                  {card.features.map((features) => (
+                    <div key={features} className="flex gap-2 items-center">
+                      <Check className="text-muted-foreground" />
+                      <p>{features}</p>
+                    </div>
+                  ))}
                 </div>
                 <Link
-                  href={`/agency?plan=${card.id}`}
+                  href={`/agency?plan=${card.priceId}`}
                   className={clsx(
                     "w-full text-center bg-primary p-2 rounded-md",
                     {
-                      "!bg-muted-foreground":
-                        card.nickname !== "Unlimited Saas",
+                      "!bg-muted-foreground": card.title !== "Unlimited Saas",
                     }
                   )}
                 >
@@ -107,45 +90,6 @@ export default async function Home() {
               </CardFooter>
             </Card>
           ))}
-          <Card className={clsx("w-[300px] flex flex-col justify-between")}>
-            <CardHeader>
-              <CardTitle
-                className={clsx({
-                  "text-muted-foreground": true,
-                })}
-              >
-                {pricingCards[0].title}
-              </CardTitle>
-              <CardDescription>{pricingCards[0].description}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <span className="text-4xl font-bold">$0</span>
-              <span>/ month</span>
-            </CardContent>
-            <CardFooter className="flex flex-col  items-start gap-4 ">
-              <div>
-                {pricingCards
-                  .find((c) => c.title === "Starter")
-                  ?.features.map((feature) => (
-                    <div key={feature} className="flex gap-2">
-                      <Check />
-                      <p>{feature}</p>
-                    </div>
-                  ))}
-              </div>
-              <Link
-                href="/agency"
-                className={clsx(
-                  "w-full text-center bg-primary p-2 rounded-md",
-                  {
-                    "!bg-muted-foreground": true,
-                  }
-                )}
-              >
-                Get Started
-              </Link>
-            </CardFooter>
-          </Card>
         </div>
       </section>
     </>
